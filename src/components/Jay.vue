642cc69862ab97b2c675246ff2d38183c15064a8
lyz1874 <a1025065047@163.com>
2022-12-14 16:25:12 +0800

    欢迎，登录，注册

---
+++ src/components/Jay.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="login">
+    <div id="w1" class="act-photo" style="position:relative;height:300rpx;width:100%;margin:auto">
+	<div id="w2" class="vert-center text-center" style="height:150rpx; margin-top:700px;">
+		<el-button class="cu-btn bg-black text-white shadow-blur text-xs" 
+    style="opacity: 0.7;width:150rpx;margin-right:140px;font-size:30px;"
+    @click="linkToLogin()" >登录</el-button>
+
+		<el-button class="margin-left cu-btn bg-black text-white shadow-blur text-xs"
+     style="opacity: 0.7;width:150rpx;font-size:30px;" 
+     @click="linkToRegister()">注册</el-button>
+	</div>
+</div>
+    </div>
+  </template>
+  
+  <style scoped>
+
+.text-center{
+	text-align:center;
+}
+.vert-center{
+	position:absolute;
+	top:0;
+	bottom:0;
+	left: 0;
+    margin: auto;
+    right: 0;
+}
+
+
+
+
+.el-button--login{
+    background: white;
+    border-color: white;
+    color: black;
+    font-size: 24px;
+    margin-right: 60px;
+  }
+
+  .el-button--register{
+    background: white;
+    border-color: white;
+    color: black;
+    font-size: 24px;
+    
+  }
+
+.login{width: 100%;
+  height: 100vh;
+  background-size: 100% 100%;
+  background-position: center center;
+  background-image: url('../img/welcome.jpg');
+  overflow: auto;
+  position: relative;}
+
+</style>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            T: []
+        }
+    },
+  methods: {
+    linkToLogin() {
+      this.$router.push('/Login');
+    },
+      linkToRegister() {
+        this.$router.push('/Register');
+      }
+    },
+    mounted(){
+        axios.get("https://www.fastmock.site/mock/e31676a60c7bc77b9f15db12616b2efc/Topic/topic").then(res => {
+            this.T = res.data;
+          console.log(this.T);
+          
+        })
+    }
+}
+
+/*export default defineComponent({
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    Register,
+    Login,
+  },
+  methods: {
+    doLogin() { this.$router.push("/Home");},
+    doRegister(){}
+  }
+})*/
+</script>
+
+
+  
---
+++ src/components/userInfoEdit.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="personalCenter">
+    <el-dialog :visible.sync="examineBtn"> 
+          姓名
+          <el-input
+            v-model="input5"
+            type="text"
+            placeholder="请输入您的姓名"
+            show-word-limit
+          ></el-input>
+        性别
+        <br/>
+          <el-select v-model="value" placeholder="请选择您的性别">
+          <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+          </el-option>
+          </el-select>
+          <br/>
+          职业
+          <el-input 
+          v-model="input7" 
+          type="text"
+          placeholder="请输入您的职业">
+          </el-input>
+          学校
+          <el-input
+            v-model="input8"
+            type="text"
+            placeholder="请输入您的学校"
+            maxlength="20"
+            show-word-limit
+          ></el-input>
+          地区
+          <el-input
+            v-model="input9"
+            type="text"
+            placeholder="请输入您的地区"
+            maxlength="100"
+            show-word-limit
+          ></el-input>
+          年龄
+          <el-input
+            v-model="input10"
+            type="number"
+            placeholder="请选择您的年龄"
+            show-word-limit
+          ></el-input>
+          <el-button-group style="float: right; padding: 8px 0" type="text">
+            <el-button type="primary" size="medium" round>保存</el-button>
+            <br/>
+          </el-button-group>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PersonalCenter',
+    data() {
+        return {
+            text: '',
+            input1: '',
+            input2: '',
+            input3: '',
+            input4: '',
+            input5: '',
+            input6: '',
+            input7: '',
+            input8: '',
+            input9: '',
+            input10: 0,
+            circleUrl:
+                'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+            sizeList: ['large'],
+            tableData: [
+                {
+                    date: '2016-05-03',
+                    name1: 'Wechat',
+                    name2: 'Github',
+                },
+            ],
+            examineBtn: false,
+            options: [{
+                value: '选项1',
+                label: '男'
+            }, {
+                value: '选项2',
+                label: '女'
+            }]
+      }
+    },
+    methods: {
+      handleEdit(index, row) {
+        console.log(index, row)
+      },
+      handleDelete(index, row) {
+        console.log(index, row)
+      },
+      init() {
+            console.log("这是详情组件");
+            this.examineBtn = true;
+      },
+    },
+  }
+</script>
+
+<style scoped>
+</style>
+
